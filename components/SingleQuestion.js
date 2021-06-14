314a799f77dff06b4598a6bf15697f959827bb69
Ansh <ansh1999arora@gmail.com>
2021-06-14 05:35:35 +0530

    add og meta tags anf favicon

--- components/SingleQuestion.js
+++ components/SingleQuestion.js
@@ -6,35 +6,53 @@ import {
   AccordionPanel,
   AccordionIcon,
   Box,
+  Image,
 } from "@chakra-ui/react";
 
 const SingleQuestion = ({ question }) => {
   return (
-    <AccordionItem>
-      <Box>
-        <AccordionButton
-          as={Flex}
-          justifyContent="space-between"
-          p="30px 15px 30px 5px"
-        >
-          <Text color="primary" fontWeight="semibold" minW="250px">
-            {question.type}
-          </Text>
+    <AccordionItem cursor="pointer">
+      {({ isExpanded }) => (
+        <>
+          <Box>
+            <AccordionButton
+              as={Flex}
+              justifyContent="space-between"
+              p="30px 15px 30px 5px"
+            >
+              <Text
+                color="primary"
+                fontWeight="semibold"
+                minW="250px"
+                display={{ base: "none", md: "block" }}
+              >
+                {question.type}
+              </Text>
 
-          <Text fontWeight="semibold" flex={1}>
-            {question.question}
-          </Text>
+              <Text fontWeight="semibold" flex={1}>
+                {question.question}
+              </Text>
+              {isExpanded ? (
+                <Image src="minus.gif" zIndex="-1" />
+              ) : (
+                <Image src="plus.png" zIndex="-1" />
+              )}
+            </AccordionButton>
+          </Box>
 
-          <AccordionIcon w="10%" />
-        </AccordionButton>
-      </Box>
-      <AccordionPanel pb={4} mr="35px">
-        {question.answer.map((answer, i) => (
-          <Text key={i} mr="40px" ml="238px">
-            {answer.data}
-          </Text>
-        ))}
-      </AccordionPanel>
+          <AccordionPanel pb={4} mr="35px">
+            {question.answer.map((answer, i) => (
+              <Text
+                key={i}
+                mr={{ base: 0, md: "30px" }}
+                ml={{ base: 0, md: "238px" }}
+              >
+                {answer.data}
+              </Text>
+            ))}
+          </AccordionPanel>
+        </>
+      )}
     </AccordionItem>
   );
 };
